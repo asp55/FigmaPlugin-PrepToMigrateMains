fba30eed599778be9a3d6a4f95b1447ab823e41d
Andrew Parnell <aparnell@gmail.com>
2022-01-14 23:07:05 -0800

    Created timer function to measure performance and figured out counting up instances

--- code.js
+++ code.js
@@ -1,1 +1,76 @@
-throw new Error("This plugin template uses Typescript. Follow the instructions in `README.md` to generate `code.js`.")
+console.log("Start PrepToMigrate");
+const timers = {
+    _timers: {},
+    start: (name) => {
+        const now = Date.now();
+        timers._timers[name] = now;
+    },
+    end: (name) => {
+        if (!timers._timers[name]) {
+            console.error("No such timer");
+            return 0;
+        }
+        else {
+            const now = Date.now();
+            const ellapsed = now - timers._timers[name];
+            console.log(`${name}: ${ellapsed}ms`);
+        }
+    }
+};
+timers.start("main");
+timers.start("indexing components");
+const instanceCounts = new Map;
+const componentSetComponents = new Map;
+const freestandingComponents = [];
+figma.currentPage.findAllWithCriteria({
+    types: ['COMPONENT']
+}).forEach(n => {
+    instanceCounts.set(n, 0);
+    if (n.parent.type === "COMPONENT_SET") {
+        if (!componentSetComponents.has(n.parent))
+            componentSetComponents.set(n.parent, []);
+        componentSetComponents.get(n.parent).push(n);
+    }
+    else {
+        freestandingComponents.push(n);
+    }
+});
+timers.end("indexing components");
+timers.start("counts");
+console.log("Starting counts");
+function countComponentChildren(node) {
+    function countChildrenRecursive(toCount) {
+        if ("children" in toCount)
+            toCount.children.forEach(c => {
+                if (c.type === "INSTANCE")
+                    instanceCounts.set(node, instanceCounts.get(node) + 1);
+                countChildrenRecursive(c);
+            });
+    }
+    countChildrenRecursive(node);
+}
+const toCount = instanceCounts.keys();
+function countNext() {
+    const { value, done } = toCount.next();
+    if (!done) {
+        const countWhat = value;
+        countComponentChildren(countWhat);
+        console.log(`Done counting instances in ${countWhat.name}, onto next in 5ms`);
+        setTimeout(countNext, 5);
+    }
+    else {
+        console.log(instanceCounts);
+        console.log(componentSetComponents);
+        console.log(freestandingComponents);
+        allDone();
+    }
+}
+countNext();
+function allDone() {
+    timers.start("counts");
+    timers.end("main");
+    console.log("PrepToMigrate Done");
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    figma.closePlugin();
+}
--- code.ts
+++ code.ts
@@ -1,21 +1,94 @@
-// This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5
+console.log("Start PrepToMigrate");
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
 
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
+const timers = {
+  _timers: {},
+  start: (name:string)=>{
+    const now = Date.now();
+    timers._timers[name] = now;
+  },
+  end: (name:string)=>{
+    if(!timers._timers[name]) {
+      console.error("No such timer"); 
+      return 0;
+    }
+    else {
+      const now = Date.now();
+      const ellapsed = now - timers._timers[name];
+      console.log(`${name}: ${ellapsed}ms`)
+    }
+  }
 }
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
 
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
-figma.closePlugin();
+timers.start("main");
+timers.start("indexing components");
+
+
+
+const instanceCounts:Map<ComponentNode, number> = new Map
+const componentSetComponents:Map<ComponentSetNode, ComponentNode[]> = new Map
+const freestandingComponents:ComponentNode[] = []
+
+
+figma.currentPage.findAllWithCriteria({
+  types: ['COMPONENT']
+}).forEach(n=>{
+  instanceCounts.set(n, 0);
+  if(n.parent.type==="COMPONENT_SET") {
+    if(!componentSetComponents.has(n.parent)) componentSetComponents.set(n.parent, []);
+    componentSetComponents.get(n.parent).push(n);
+  }
+  else {
+    freestandingComponents.push(n);
+  }
+});
+
+timers.end("indexing components");
+timers.start("counts")
+console.log("Starting counts");
+
+function countComponentChildren(node:ComponentNode):void {
+  function countChildrenRecursive(toCount:SceneNode):void {
+    if("children" in toCount) toCount.children.forEach(c=>{
+      if(c.type==="INSTANCE") instanceCounts.set(node, instanceCounts.get(node)+1);
+      countChildrenRecursive(c);
+    })
+  }
+
+  countChildrenRecursive(node);
+}
+
+const toCount = instanceCounts.keys();
+
+function countNext() {
+  const {value, done} = toCount.next();
+  if(!done) {
+    
+    const countWhat = value;
+    countComponentChildren(countWhat);
+    console.log(`Done counting instances in ${countWhat.name}, onto next in 5ms`);
+    setTimeout(countNext,5)
+  }
+  else {
+    console.log(instanceCounts);
+    console.log(componentSetComponents);
+    console.log(freestandingComponents);
+    allDone();
+  }
+}
+
+countNext();
+
+
+function allDone() {
+
+  timers.start("counts")
+  timers.end("main");
+  console.log("PrepToMigrate Done");
+  
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
+  
+
+}
